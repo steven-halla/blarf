850baaff9ae19ed5017b073d46c539895fffd1be
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-10-14 16:09:32 -0700

    first

---
+++ App.tsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import {Text, View} from "react-native";
+
+
+const App = () => {
+  return(
+    <View>
+        <Text>Hi I'm the start of the app</Text>
+    </View>
+  );
+};
+
+export default App;
